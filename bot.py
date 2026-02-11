dc13f9121ca326f07ad6b83328af06b2c91f1641
zishanz3 <60655030+zishanz3@users.noreply.github.com>
2026-02-11 21:34:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,7 @@ import os
 import discord
 import asyncio
 from discord.ext import commands
-from dotenv import load_dotenv
 
-load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 #DEV_GUILD_ID = 729007907131359322  # <-- your server ID
